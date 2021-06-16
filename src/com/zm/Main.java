1042c2733167573e4a7dd5dee55b3807c213aaa7
zmmarina <zmarina.hm@gmail.com>
2021-06-16 08:59:00 -0300

    discount exercise

--- src/com/zm/Main.java
+++ src/com/zm/Main.java
@@ -6,15 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
-        int tshirt = 50;
-
-        System.out.println("how many t-shirts do you want?");
-        int qnt = sc.nextInt();
-
-        double choice = (qnt == 2) ? tshirt * 0.8 * qnt : tshirt * qnt;
-        System.out.println(choice);
 
     }
 }
